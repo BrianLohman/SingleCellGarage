dcba1324ac9a51acb3a8a7503645289641435650
Brian Lohman <brian.keith.lohman@gmail.com>
2021-05-12 13:05:23 -0600

    initial

---
+++ R/gsea.R
@@ -0,0 +1,37 @@
+#' Covert the ouptut of Seurat's FindMarkers to a table that hciR's fgsea_all can use
+#'
+#' This function assumes that a user has run Seurat's FindMarkers to
+#'      assess differential expression between clusters and would like to
+#'      next run GSEA with hciR's fgsea_all and gene to pathway data from
+#'      hciRdata.
+#'
+#' @param markers Table from Seurat's FindMarkers
+#' @param contrast The name of the contrast
+#' @param database Table of genes to use when gathering annotations. Default is hciRdata::human100
+#'
+#' @examples
+#' sc_results = prep_sc_DE_for_fgsea(markers = markers, contrast = "cluster 4 vs. cluster 0")
+#' fgsea_all(res = sc_results, gsets = msig_pathways$KEGG, FDR = 0.1, nperm = 10000)
+#'
+#' @export
+
+prep_sc_DE_for_fgsea = function(markers = markers, contrast = NULL, database = hciRdata::human100){
+    # check that contrast is named
+    if(is.null(contrast)){
+    stop("contrast must be name: e.g., cluster_1 vs cluster_2")
+    }
+
+    # set row names from markers (gene names) to a column
+    markers = data.frame(gene_name = row.names(markers), markers)
+
+    # reformat for fgsea_all and merge in the annotations
+    res = merge(markers[, c(1:3,6)], database[, c(1:4,8)], by = "gene_name")
+
+    # set column name of the log2fold change to match expected by fgsea_all
+    colnames(res) = gsub("avg_log2FC", "log2FoldChange", colnames(res))
+
+    # convert to list as expected by fgsea_all and set contrast name
+    res = list(res)
+    names(res) = contrast
+    return(res)
+}
---
+++ R/load.R
@@ -0,0 +1,162 @@
+#' Build file manifest from 10X cellranger-atac
+#'
+#' Assumes that each sample is in its own folder. Pattern match to get multiple samples
+#' Returns named list where each sample has paths to peaks.bed, singlecell.csv, and fragments file
+#' Also possible to build list or make corrections manually
+#'
+#' @param pattern Common string among all samples
+#'
+#' @examples
+#' m = build_manifest(pattern = "18655")
+#'
+#' @export
+
+build_manifest <- function(pattern = NULL){
+  # sample ID: one directory for each sample
+  s = list.files(pattern = pattern)
+  l = vector(mode = "list", length = length(s))
+  names(l) = s
+
+  # peaks.bed
+  for(i in 1:length(l)){
+    l[[i]]["peaks"] = list.files(path = paste("./", names(l)[i],  sep = ""), pattern = "peaks.bed", full.names = TRUE)
+  }
+
+  # singlecell.csv, the metadata
+  for(i in 1:length(l)){
+    l[[i]]["metadata"] = list.files(path = paste("./", names(l)[i],  sep = ""), pattern = "singlecell.csv", full.names = TRUE)
+  }
+
+  # fragments file, get only first match (second is tabix index)
+  for(i in 1:length(l)){
+    l[[i]]["fragments"] = list.files(path = paste("./", names(l)[i],  sep = ""), pattern = "fragments.tsv.gz", full.names = TRUE)[1]
+  }
+
+  return(l)
+}
+
+#' Read in multiple 10X ATAC samples with Signac and merge with common peak set
+#'
+#' Takes file manifest from build_manifest
+#' following preffered merge procedure in Signac: https://satijalab.org/signac/articles/merging.html#merge-fragment-files-1
+#'
+#' @param manifest The file manifest from build_manifest or a list of named lists where each entry is a sample with peaks.bed, singlecell.csv and fragments
+#' @param genome The version of the genome that samples were mapped to
+#' @param project Name to give merged sample set
+#'
+#' @examples
+#' srt = build_10X_atac(manifest = m, genome = "GRCh38", project = "A6440")
+#'
+#' @export
+
+build_10x_atac <- function(manifest = NULL, genome = NULL, project = NULL){
+  if(is.null(manifest)){
+    stop("file manifest undefined")
+  }
+
+  if(is.null(project)){
+    stop("project undefined")
+  }
+
+  if(is.null(genome)){
+    stop("genome undefined")
+  }
+
+  # assign file manifest
+  m = manifest
+
+  # read in peaks files to list
+  print("building peaks list")
+  peaks_list = vector(mode = "list", length = length(m))
+  names(peaks_list) = names(m)
+
+  for(i in names(peaks_list)){
+    peaks_list[[i]] = read.table(
+      file = as.character(m[[i]]["peaks"]),
+      col.names = c("chr", "start", "end")
+    )
+  }
+
+  # convert to genomic ranges
+  print("converting to genomic ranges and building unifed set")
+  gr_list = vector(mode = "list", length = length(m))
+  names(gr_list) = names(m)
+
+  for(i in names(gr_list)){
+    gr_list[[i]] <- makeGRangesFromDataFrame(peaks_list[[i]])
+  }
+
+  # Create a unified set of peaks to quantify in each dataset
+  combined.peaks <- reduce(GRanges(Reduce("c", gr_list)))
+
+  # Filter out bad peaks based on length
+  print("prefiltering")
+  peakwidths <- width(combined.peaks)
+  combined.peaks <- combined.peaks[peakwidths  < 10000 & peakwidths > 20]
+
+  # load metadata
+  print("building metadata list")
+  metadata_list = vector(mode = "list", length = length(m))
+  names(metadata_list) = names(m)
+
+  for(i in names(metadata_list)){
+    metadata_list[[i]] = read.table(
+      file = as.character(m[[i]]["metadata"]),
+      stringsAsFactors = FALSE,
+      sep = ",",
+      header = TRUE,
+      row.names = 1
+    )[-1, ] # remove the first row
+  }
+
+  # perform an initial filtering of low count cells
+  print("filtering low count cells")
+  for(i in names(metadata_list)){
+    metadata_list[[i]] = metadata_list[[i]][metadata_list[[i]]$passed_filters > 200, ]
+  }
+
+  # create fragment objects
+  print("building fragment list")
+  fragments_list = vector(mode = "list", length = length(m))
+  names(fragments_list) = names(m)
+
+  for(i in names(fragments_list)){
+    fragments_list[[i]] <- CreateFragmentObject(
+      path = as.character(m[[i]]["fragments"]),
+      cells = rownames(metadata_list[[i]])
+    )
+  }
+
+  # quantify peaks in each data set
+  print("quantifying peaks in parallel")
+  build_feature_matrix <- function(sample){
+    FeatureMatrix(
+      fragments = fragments_list[[sample]],
+      features = combined.peaks,
+      cells = rownames(metadata_list[[sample]])
+    )
+  }
+  counts_list = mclapply(names(m), build_feature_matrix)
+  names(counts_list) = names(m)
+
+  # Create Seurat objects
+  print("building Seurat objects")
+  seurat_list = vector(mode = "list", length = length(m))
+  names(seurat_list) = names(m)
+
+  for(i in names(seurat_list)){
+    tmp = CreateChromatinAssay(counts_list[[i]], fragments = fragments_list[[i]], genome = genome)
+    seurat_list[[i]] = CreateSeuratObject(tmp, assay = "ATAC", project = i, meta.data = metadata_list[[i]])
+  }
+
+  # merge all datasets, adding a cell ID to make sure cell names are unique
+  print("merging Seurat objects")
+  srt <- merge(
+    x = seurat_list[[1]],
+    y = seurat_list[2:length(m)],
+    add.cell.ids = names(m),
+    project = project
+  )
+
+  return(srt)
+}
