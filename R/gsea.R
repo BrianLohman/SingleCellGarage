5e2fb4df578453cbeac7f70c4e6bd20077e95b47
Brian Lohman <brian.keith.lohman@gmail.com>
2022-02-15 10:24:16 -0700

    add option to convert to human homolog

--- R/gsea.R
+++ R/gsea.R
@@ -8,6 +8,7 @@
 #' @param markers Table from Seurat's FindMarkers
 #' @param contrast The name of the contrast
 #' @param database Table of genes to use when gathering annotations. Default is hciRdata::human100
+#' @param human_homolog Convert the gene names to human homologs. Default is FALSE
 #'
 #' @examples
 #' \dontrun{
@@ -17,17 +18,23 @@
 #'
 #' @export
 
-prep_sc_DE_for_fgsea = function(markers = markers, contrast = NULL, database = hciRdata::human100){
+prep_sc_DE_for_fgsea = function(markers = markers, contrast = NULL, database = hciRdata::human100, human_homolog = FALSE){
     # check that contrast is named
     if(is.null(contrast)){
-    stop("contrast must be name: e.g., cluster_1 vs cluster_2")
+        stop("contrast must be name: e.g., cluster_1 vs cluster_2")
     }
 
     # set row names from markers (gene names) to a column
     markers = data.frame(gene_name = row.names(markers), markers)
 
     # reformat for fgsea_all and merge in the annotations
-    res = merge(markers[, c(1:3,6)], database[, c(1:4,8)], by = "gene_name")
+    if(human_homolog == FALSE){
+        res = merge(markers[, c(1:3,6)], database[, c(1:4,8)], by = "gene_name")
+    } else {
+        res = merge(markers[, c(1:3,6)], database[, c(1:4,8,10)], by = "gene_name")
+        res$gene_name = res$human_homolog
+        res = res[!is.na(res$gene_name) ,]
+    }
 
     # set column name of the log2fold change to match expected by fgsea_all
     colnames(res) = gsub("avg_log2FC", "log2FoldChange", colnames(res))
